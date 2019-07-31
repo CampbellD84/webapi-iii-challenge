e486f8f0280685312180ba7768d9a1cd2a45e92d
Douglas Campbell <dacampbelltbone@gmail.com>
2019-07-30 23:26:43 -0400

    Basic server functionality working, fixing middleware errors

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-const express = 'express';
+const express = require("express");
+const userRouter = require("./users/userRouter");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+server.use(logger);
+server.use(express.json());
+server.use("/api/users", userRouter);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
-};
+  console.log(
+    `${req.method} request received,  ${req.url}, ${new Date().toISOString()}`
+  );
+  next();
+}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,136 @@
-const express = 'express';
+const express = require("express");
+const Users = require("./userDb");
+const Posts = require("../posts/postDb");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", validateUser, async (req, res) => {
+  try {
+    const user = await Users.insert(req.body);
 
+    res.status(201).json(user);
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({ message: "Error adding new user." });
+  }
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post("/:id/posts", validatePost, async (req, res) => {
+  const postInfo = { ...req.body, post_id: req.params.id };
 
+  try {
+    const post = await Posts.insert(postInfo);
+    res.status(201).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.get('/', (req, res) => {
-
+router.get("/", async (req, res) => {
+  try {
+    const users = await Users.get(req.query);
+    res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error retrieving users." });
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", validateUserId, async (req, res) => {
+  try {
+    const user = await Users.getById(req.params.id);
 
+    if (user) {
+      res.status(200).json(user);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error retrieving user." });
+  }
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get("/:id/posts", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const posts = await Users.getUserPosts(id);
 
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error getting posts for user." });
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", validateUserId, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteUser = await Users.remove(id);
 
+    res.status(200).json({ message: "User has been removed" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "User could not be removed." });
+  }
 });
 
-router.put('/:id', (req, res) => {
-
+router.put("/:id", validateUserId, async (req, res) => {
+  try {
+    const user = await Users.update(req.params.id, req.body);
+    if (user) {
+      res.status(200).json(user);
+    }
+  } catch (error) {
+    console.log(500).json({ message: "Error updating the user." });
+  }
 });
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
+  try {
+    const { id } = req.params;
+    const user = await Users.getById(id);
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      res.status(400).json({ message: "invalid user id" });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+}
 
-};
+async function validateUser(req, res, next) {
+  try {
+    const userBody = await req.body;
 
-function validateUser(req, res, next) {
+    if (userBody.name === "") {
+      res.status(400).json({ message: "Missing required name field." });
+    } else if (userBody.body === "") {
+      res.status(400).json({ message: "Missing required text field." });
+    } else {
+      next();
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+}
 
-};
-
-function validatePost(req, res, next) {
-
-};
+async function validatePost(req, res, next) {
+  const post = await req.body;
+  try {
+    if (!post) {
+      res.status(400).json({ message: "Missing post data." });
+    } else if (post.text === "") {
+      res.status(400).json({ message: "Missing required text field." });
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 module.exports = router;
